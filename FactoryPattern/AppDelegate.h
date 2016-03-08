2b2e6726987f082e9ff56aa1fb6828a8e9ffc643
henanzhg <854702773@qq.com>
2016-03-08 20:04:44 +0800

    Initial Commit

---
+++ FactoryPattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FactoryPattern
+//
+//  Created by mac on 3/8/16.
+//  Copyright © 2016 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
